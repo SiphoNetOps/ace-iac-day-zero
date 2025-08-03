ff005f260ae892b3f4d114d4b940dbd2de038903
SiphoNetOps <sipho.netops.n1@gmail.com>
2025-08-03 21:46:29 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,5 @@
 // ACE-IAC Core Aviatrix Infrastructure
 
-provider "aviatrix" {
-  controller_ip           = var.controller_ip
-  username                = var.controller_username
-  password                = var.controller_password
-  skip_version_validation = true
-}
-
 resource "aviatrix_account" "azure_account" {
   account_name         = var.azure_account_name
   cloud_type           = 8
