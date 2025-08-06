5347e21ec75ac6109bce9302f8b2885075b70997
SiphoNetOps <sipho.netops.n1@gmail.com>
2025-08-06 20:28:10 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -17,3 +17,17 @@ provider "azurerm" {
   client_secret              = var.azure_client_secret
   tenant_id                  = var.azure_tenant_id
 }
+
+data "aws_vpc" "transit_vpc" {
+  id = "vpc-0a80b600f7c012089"
+}
+
+data "aws_vpc" "spoke_vpc" {
+  id = "vpc-0eaf7fbd76456f319"
+}
+
+data "aws_subnet" "spoke_subnet" {
+  id = "subnet-050e05b8f6dcfed9f"
+}
+
+
